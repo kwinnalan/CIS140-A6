83f662d4cc05c9c65f05e760f779a62f6dacb195
kwinn <kdanforth@student.ncmcih.edu>
2021-11-23 00:25:28 -0500

    added reporting and age calculation

--- a6_a.pl
+++ a6_a.pl
@@ -7,12 +7,15 @@ use 5.14.2;
 use warnings;
 
 my ($welcome, $continue, $counter, $menuChoice);
-my (@patient, @patients, @homeMenu);
-use constant MAX_RECURSION => 5;
+my (@patients, @homeMenu);
+my ($day, $month, $year) = (localtime)[3,4,5];
+
+use constant COLUMBS => 8;
 use constant TRUE => 1;
 
 sub main {
 	system ("clear");
+    setDate();
     setWelcome();
     setHomeMenu();
     displayWelcome();
@@ -24,8 +27,17 @@ sub main {
 
 main();
 
+sub setDate {
+if(defined $counter){
+}else{
+    $day = $day;
+    $month = $month+1;
+    $year = $year+1900;
+    }
+}
+
 sub setWelcome {
-    $welcome = "Welcome, below are your menu options:";
+    $welcome = "$month/$day/$year\n Welcome, below are your menu options:";
 }
 
 sub setHomeMenu {
@@ -70,13 +82,14 @@ sub processMenuChoice {
 }
 
 sub addPatient {
-my ( $dob, $insuranceCarrier, $Ailment);
 	setCounter();
     setPatientID();
 	setLastName();
 	setFirstName();
     setMiddleName();
-    push(@patients, (@patient));
+    setDOB();
+    setInsuranceCarrier();
+    setAilment();
 	setContinue();
 	if ($continue == TRUE) {
 		addPatient();
@@ -85,16 +98,42 @@ my ( $dob, $insuranceCarrier, $Ailment);
     }
 }
 
-sub processPatientData {
-        #for my $i (@patients){
-my @patientOne = $patients[0];
-#my @patientOne = $patients[$i];
-# for (my $j = 0; $j <= $#patientOne; $j++){
-            my $ID = $patientOne[0];
-            print "$ID";
+sub processPatientData {        
+my $dailyReportFile = "dailyReport$month-$day-$year.txt";
+open(my $dayReportFileHandle, '>>', $dailyReportFile) or die "could not open file '$dailyReportFile' $!";
+
+        for (my $i = 1; $i <= $#patients; $i++){
+               for (my $j = 0; $j < (&COLUMBS); $j++){
+                     if($j != (&COLUMBS-1)){
+                         print $dayReportFileHandle "$patients[$i][$j], ";
+                     }else{
+                         print $dayReportFileHandle "$patients[$i][$j]\n";
+                     }
+                } 
+              }              
+close $dayReportFileHandle;
+processNoInsurance();
+}
+
+sub processNoInsurance{
+my $dailyNoInsuranceFile = "dailyNoInsurence$month-$day-$year.txt";
+open(my $noInsuranceFileHandle, '>>', $dailyNoInsuranceFile) or die "could not open file '$dailyNoInsuranceFile' $!";
+    for (my $i = 1; $i <= $#patients; $i++){
+          for (my $j = 0; $j < (&COLUMBS); $j++){
+                if($j == &COLUMBS-2 && $patients[$i][$j] eq 'none'){
+                      for (my $k = 0; $k < &COLUMBS; $k++){
+                                        if($k != &COLUMBS-1){
+                                           print $noInsuranceFileHandle "$patients[$i][$k], ";
+                                        }else{
+                                             print $noInsuranceFileHandle "$patients[$i][$k]\n";
+                                        
+                                        }
+                                }
+                        }
+                }
         }
-        #}
-        #}
+              close $noInsuranceFileHandle;
+}
 
 sub setCounter {
 	if (defined $counter) {
@@ -123,7 +162,7 @@ sub setContinue {
 
 sub setPatientID {
 my $patientID = ($counter + 1000);
-@patient = $patientID;
+$patients[$counter][(&COLUMBS - &COLUMBS)] = $patientID;
 }
 
 sub setLastName {
@@ -139,7 +178,7 @@ my $lastName;
 			sleep 1;
 			system ("clear");
 		}else {
-            push(@patient, ($lastName));
+            $patients[$counter][&COLUMBS-&COLUMBS+1] = $lastName;
         }
 	}
 }
@@ -157,7 +196,7 @@ my $firstName;
 			sleep 1;
 			system ("cls");
 		}else {
-            push(@patient, ($firstName));
+            $patients[$counter][&COLUMBS-&COLUMBS+2] = $firstName;
         }
 	}
 }
@@ -175,8 +214,117 @@ my $middleName;
 			sleep 1;
 			system ("clear");
 		}else {
-            push(@patient, ($middleName));
+            $patients[$counter][&COLUMBS-&COLUMBS+3] = $middleName;
         }
 	}
 }
 
+sub setDOB {
+my $birthMonth = setBirthMonth();
+my $birthDay = setBirthDay();
+my $birthYear = setBirthYear();
+my $DOB = "$birthMonth/$birthDay/$birthYear";
+$patients[$counter][&COLUMBS-&COLUMBS+4] = $DOB;
+calculateAge($birthMonth, $birthDay, $birthYear);
+}
+
+sub calculateAge {
+my ($birthMonth, $birthDay, $birthYear) = @_;
+my $age = $year-$birthYear;
+if($birthMonth > $month){
+    $age = $age-1;
+}elsif($birthMonth == $month && $birthDay > $day){
+    $age = $age-1;
+}
+$patients[$counter][&COLUMBS-&COLUMBS+5] = $age;
+}
+
+sub setBirthMonth {
+my $birthMonth;
+	if (!(defined $birthMonth)) {
+		$birthMonth = 0;
+	}
+	while ($birthMonth < 1 || $birthMonth > 12) {
+		print "What is the patients birth month? ";
+		chomp ($birthMonth = <STDIN>);
+		if ($birthMonth < 1 || $birthMonth > 12) {
+			say "Incorrect input. Please try again";
+			sleep 1;
+			system ("clear");
+		}else {
+            return $birthMonth;
+        }
+	}
+}
+
+sub setBirthDay {
+my $birthDay;
+	if (!(defined $birthDay)) {
+		$birthDay = 0;
+	}
+	while ($birthDay < 1 || $birthDay > 31) {
+		print "What is the patients birth day? ";
+		chomp ($birthDay = <STDIN>);
+		if ($birthDay < 1 || $birthDay > 31) {
+			say "Incorrect input. Please try again";
+			sleep 1;
+			system ("clear");
+		}else {
+            return $birthDay;
+        }
+	}
+}
+
+sub setBirthYear {
+my $birthYear;
+	if (!(defined $birthYear)) {
+		$birthYear = 0;
+	}
+	while ($birthYear < 1900 || $birthYear > 2021) {
+		print "What is the patients birth year? ";
+		chomp ($birthYear = <STDIN>);
+		if ($birthYear < 1900 || $birthYear > 2021) {
+			say "Incorrect input. Please try again";
+			sleep 1;
+			system ("clear");
+		}else {
+            return $birthYear;
+        }
+	}
+}
+
+sub setInsuranceCarrier {
+my $insuranceCarrier;
+	if (!(defined $insuranceCarrier)) {
+		$insuranceCarrier = 1;
+	}
+	while ($insuranceCarrier !~ /^[a-zA-Z]{2,20}$/x) {
+		print "What is the patients insurance carrier?(if uninsured type 'none') ";
+		chomp ($insuranceCarrier = <STDIN>);
+		if ($insuranceCarrier !~ /^[a-zA-Z]{2,20}$/x) {
+			say "Incorrect input. Please try again";
+			sleep 1;
+			system ("clear");
+		}else {
+            $patients[$counter][&COLUMBS-&COLUMBS+6] = $insuranceCarrier;
+        }
+	}
+}
+
+sub setAilment {
+my $ailment;
+	if (!(defined $ailment)) {
+		$ailment = 1;
+	}
+	while ($ailment !~ /^[a-zA-Z]{2,20}$/x) {
+		print "What is the patients ailment? ";
+		chomp ($ailment = <STDIN>);
+		if ($ailment !~ /^[a-zA-Z]{2,20}$/x) {
+			say "Incorrect input. Please try again";
+			sleep 1;
+			system ("clear");
+		}else {
+            $patients[$counter][&COLUMBS-&COLUMBS+7] = $ailment;
+        }
+	}
+}
